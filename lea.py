e253b738c927704e60be0d7627aa8fcd18ef857b
Ivanna Navrotska <navrockaivanna15@gmail.com>
2025-05-27 21:18:05 +0300

    add delta and shifts

--- lea.py
+++ lea.py
@@ -1,1 +1,15 @@
-print('hello LEA!')
+delta = [
+    0xc3efe9db,  
+    0x44626b02,  
+    0x79e27c8a,  
+    0x78df30ec,  
+    0x715ea49e,  
+    0xc785da0a]
+
+
+def l_shift(x, n):
+    return ((x << n) | (x >> (32 - n))) & 0xFFFFFFFF
+
+def r_shift(x, n):
+    return ((x >> n) | (x << (32 - n))) & 0xFFFFFFFF
+
